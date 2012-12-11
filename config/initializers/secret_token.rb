ee4106df1ab59a581f5bf42a8ee09d6d0081e1ad
josisusan <josisusan@gmail.com>
2012-12-11 20:19:31 +0545

    Rails application intialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Besthiring::Application.config.secret_token = 'c7b815d3a1e51eba52ab3bb532dd99350482ab3e74b1e88c3e3869a956f904d7d2d302e7c42d217a08635c92607d0d607d8ca0b08b7b950e67047c08c0e81774'
