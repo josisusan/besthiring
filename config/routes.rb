848f520df76c31ecb2949472fc5e81fa6ad76b52
josisusan <josisusan@gmail.com>
2012-12-11 20:28:20 +0545

    --help

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Besthiring::Application.routes.draw do
+  get "jobs/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -38,7 +40,7 @@ Besthiring::Application.routes.draw do
   #       get 'recent', :on => :collection
   #     end
   #   end
-
+  resources :jobs
   # Sample resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
@@ -48,7 +50,7 @@ Besthiring::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'jobs#index'
 
   # See how all your routes lay out with "rake routes"
 
